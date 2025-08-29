b8deb3ca73a5f749a84a9f802611c3417f8b46b5
otherdevhhh <otherdevhhh@users.noreply.github.com>
2025-08-29 09:43:39 +0000

    deploy: f04a99db75938452ee90824ee2beb9f2dae32779

---
+++ assets/cursor-Ce4YfDyk.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-lXatGRd6.js";import"./index-CC_faV3F.js";import"./index.es-UML9ORC1.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
