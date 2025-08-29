bd11ee3bba8f997691c46d531d1b17bcb8fbeb83
otherdev <otherdev@outlook.com>
2025-08-29 17:18:21 +0800

    [test] update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,32 @@ import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  appType: 'mpa',
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  // },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  console.info('BaseUrl:', env.VITE_BASE_URL)
+  return {
+    plugins: [
+      tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+      viteReact(),
+      tailwindcss(),
+    ],
+    appType: 'mpa',
+    // test: {
+    //   globals: true,
+    //   environment: 'jsdom',
+    // },
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
