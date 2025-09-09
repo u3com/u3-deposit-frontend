c8a6775dd0e0eb55c422df698e8b036e26b6d4d5
otherdevhhh <otherdevhhh@users.noreply.github.com>
2025-09-09 09:51:27 +0000

    deploy: b4ed6c4d796d66366df96a5f7469509149fffd65

---
+++ assets/cursor-DsMiTwFl.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-C58oLBDB.js";import"./index-B13nE6GK.js";import"./index.es-DcX3I4T2.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
