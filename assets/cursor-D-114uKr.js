81b39da01d41a9f7d757fc466e87805d509cc50b
otherdevhhh <otherdevhhh@users.noreply.github.com>
2025-09-10 05:28:55 +0000

    deploy: da91d8a77b9a4aadc07d5195642b1cedde9b296a

--- assets/cursor-D-114uKr.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-BYs6Qhjn.js";import"./index-I8PFrMGc.js";import"./index-BbP3371Q.js";import"./index.es-zE9SdjTq.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const e=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{e as cursorSvg};
