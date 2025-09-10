f579136e23d88763786e30c5d101d7925c03ffa7
otherdevhhh <otherdevhhh@users.noreply.github.com>
2025-09-10 03:11:12 +0000

    deploy: c91a2b2d2fac740adc6efeedb722b790515b6430

---
+++ assets/chevron-right-BfN3bppc.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-BYs6Qhjn.js";import"./index-I8PFrMGc.js";import"./index-BbP3371Q.js";import"./index.es-zE9SdjTq.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const m=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{m as chevronRightSvg};
